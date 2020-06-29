fa6739747b16bc111cadb4ce590f9b25c08fe486
kq <oxyzhg@163.com>
2020-06-29 23:56:17 +0800

    feat: add examples

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+const _ = require('lodash');
+
+const OFF = 'off';
+const WARN = 'warn';
+const ERROR = 'error';
+
+module.exports = _.merge(require('@dotlim/eslint-config').eslint, {
+  rules: {
+    'no-cond-assign': WARN,
+    'no-console': OFF,
+    'no-param-reassign': OFF,
+    'no-plusplus': WARN,
+    'no-unused-vars': WARN,
+    'no-restricted-syntax': OFF,
+    'no-trailing-spaces': ERROR,
+    'no-underscore-dangle': OFF,
+    'prefer-const': OFF,
+    'prefer-destructuring': OFF,
+    'no-restricted-globals': OFF,
+    'consistent-return': OFF,
+    'no-else-return': OFF,
+    'import/no-cycle': OFF,
+  },
+});
---
+++ examples/App.vue
@@ -0,0 +1,5 @@
+<template>
+  <div id="app">
+    <formulate></formulate>
+  </div>
+</template>
